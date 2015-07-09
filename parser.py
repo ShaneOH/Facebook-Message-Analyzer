cbf73464a19b1609767952c4f4547b05fbec59eb
Shane O'Hanlon <shane.ohanlon93@gmail.com>
2015-07-09 06:20:46 -0400

    parsing testMessages.htm works non-iteratively, including database storage

--- parser.py
+++ parser.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-import logging
+import logging, sqlite3
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
-# parser.py - parse HTML
+# parser.py - parse messages.htm and store the messages in a database
 
-import os
 from html.parser import HTMLParser
 
-#table = {}
+#connect to SQLite database
+messageDB = sqlite3.connect('testMessages.db')
+db = messageDB.cursor()
 
 class MyHTMLParser(HTMLParser):
     def __init__(self):
         super().__init__(convert_charrefs=True)
-        self.finished = False
         self.useless = True
         self.timestamp = ""
         self.tsFlag = False
@@ -21,7 +21,8 @@ class MyHTMLParser(HTMLParser):
         self.nFlag = False
         self.message = ""
         self.mFlag = False
-       
+        self.threadList = []
+
     def handle_starttag(self, tag, attrs):
        
         if tag == "html" or tag == "head" or tag == "meta" or tag == "li" or tag == "title" or tag == "link" or tag == "img" or tag == "body" or tag == "a" or tag == "h1" or tag == "h2": #junk html 
@@ -32,7 +33,10 @@ class MyHTMLParser(HTMLParser):
                 if name == "class":
                     if value == "message" or value == "message_header": #beginning of messages but unimportant
                         self.useless = False
-       
+                    if value == "thread": #new thread
+                        self.useless = False
+                        self.thFlag = True
+
         if tag == "span":
             for name, value in attrs:
                 if name == "class":
@@ -42,9 +46,6 @@ class MyHTMLParser(HTMLParser):
                     if value == "meta": #timestamp attached to message
                         self.useless = False
                         self.tsFlag = True
-                    if value == "thread": #new thread
-                        self.useless = False
-                        self.thFlag = True
                     
         if tag == "p": #actual message
             self.useless = False
@@ -55,19 +56,34 @@ class MyHTMLParser(HTMLParser):
 
     def handle_data(self, data):
         if self.useless == False:
-            logging.debug("We encountered some useful data: " + str(data))
-"""
+            
+            if self.thFlag == True:
+                self.thread = data
+                logging.debug("We hit a thread, setting self.thread to: " + str(self.thread))
+                if self.thread not in self.threadList:
+                    #create new table for thread
+                    db.execute('''CREATE TABLE "'''+self.thread+'''" (name text, time text, message text)''') #NOTE: not best practice, in real-world use string constructor opens up SQL Injection vulnerability. Would have to scrub string input. 
+                    self.threadList.append(self.thread)
+                self.thFlag = False
             if self.nFlag == True:
                 self.name = data
-                #logging.debug("We're setting self.name to " + str(data))
                 self.nFlag = False
             if self.tsFlag == True:
                 self.timestamp = data
-                #logging.debug("We're setting self.timestamp to " + str(data))
                 self.tsFlag = False
-"""
-messages = open('/home/shane/Documents/FacebookProject/FacebookData/html/messages.htm')
-data = messages.read()
+            if self.mFlag == True:
+                self.message = data
+                self.mFlag = False
+
+        if self.name and self.timestamp and self.message: #we've got a full message object
+            self.list = [self.name, self.timestamp, self.message]
+            self.name, self.timestamp, self.message = "","",""
+            #insert message into table
+            db.execute('''INSERT INTO "'''+self.thread+'''" VALUES (?,?,?)''', self.list)
+
+testMessages = open('/home/shane/Documents/FacebookProject/FacebookData/html/testMessages.htm')
+data = testMessages.read()
 parser = MyHTMLParser()
 parser.feed(data)
-#print(table)
+messageDB.commit()
+messageDB.close()
