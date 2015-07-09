435ca94db0ce125ef41c411ea8bc80c1c7cec261
Shane O'Hanlon <shane.ohanlon93@gmail.com>
2015-07-09 02:41:49 -0400

    parser works correctly for simple use case: pokes

---
+++ pokeParser.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+import logging
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+# parser.py - parse HTML
+
+import os
+from html.parser import HTMLParser
+
+table = {}
+
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        super().__init__(convert_charrefs=True)
+        self.finished = False
+        self.useless = True
+        self.timestamp = ""
+        self.tsFlag = False
+        self.thread = ""
+        self.thFlag = False
+        self.name = ""
+        self.nFlag = False
+        self.message = ""
+        self.mFlag = False
+       
+    def handle_starttag(self, tag, attrs):
+       
+        if tag == "html" or tag == "head" or tag == "meta" or tag == "title" or tag == "link" or tag == "img" or tag == "body" or tag == "a" or tag == "h1" or tag == "h2": #junk html 
+            self.useless = True
+
+        if tag == "div":
+            for name, value in attrs:
+                if name == "class":
+                    if value == "meta": #timestamp
+                        self.useless = False
+                        self.tsFlag = True
+           
+        if tag == "li": #poke
+            if not attrs:
+                self.useless = False
+                self.nFlag = True
+
+    def handle_endtag(self, tag):
+        self.useless = True
+
+    def handle_data(self, data):
+        if self.useless == False:
+            logging.debug("We encountered some useful data: " + str(data))
+
+            if self.nFlag == True:
+                self.name = data
+                logging.debug("We're setting self.name to " + str(data))
+                self.nFlag = False
+            
+            if self.tsFlag == True:
+                self.timestamp = data
+                logging.debug("We're setting self.timestamp to " + str(data))
+                self.tsFlag = False
+       
+        if self.name and self.timestamp:
+            logging.debug("We're now setting " + str(self.name) + " = " + str(self.timestamp))
+            table[self.name] = self.timestamp
+
+pokes = open('/home/shane/Documents/FacebookProject/FacebookData/html/pokes.htm')
+data = pokes.read()
+parser = MyHTMLParser()
+parser.feed(data)
+print(table)
